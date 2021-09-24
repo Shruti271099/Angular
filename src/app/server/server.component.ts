5b8c0c45f88f3d9f8a1168bcea29887be44a8bad
Shruti Lalotra <shrutilalotra453@gmail.com>
2021-09-24 14:18:26 +0530

    015-018

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { templateSourceUrl } from "@angular/compiler";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: '<h4></h4>',
+    templateUrl : "./server.component.html",
+})
+
+export class ServerComponent{
+
+}
