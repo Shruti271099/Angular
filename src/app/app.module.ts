0fcf2dd05f23977fb59342163b4c77b1b2912bd9
Shruti Lalotra <shrutilalotra453@gmail.com>
2021-09-27 16:19:18 +0530

    045-049-Project03Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { P1Component } from './p1/p1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    P1Component
+    HeaderComponent
   ],
   imports: [
     BrowserModule
