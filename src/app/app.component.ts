3ee189f635b02a2062736f13e547b8d5544830ab
Shruti Lalotra <shrutilalotra453@gmail.com>
2021-09-28 12:14:36 +0530

    062-068-data-binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Fifth-Data-Binding';
+  serverElements = [{type:'server', name:'my name is server',content:'This is content'}];
+  
+
+  
 }
