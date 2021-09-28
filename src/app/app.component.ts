e15d356c56b1ccd9ef6bffdc9a37bde0ad8a9b4c
Shruti Lalotra <shrutilalotra453@gmail.com>
2021-09-28 14:59:05 +0530

    069-072-event-communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,22 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Fifth-Data-Binding';
   serverElements = [{type:'server', name:'my name is server',content:'This is content'}];
+
+  onServerAdded(serverData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(bluePrintData:{serverName:string,serverContent:string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
   
 
   
--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output ,EventEmitter} from '@angular/core';
+
 
 @Component({
   selector: 'app-cockpit',
@@ -7,28 +8,22 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class CockpitComponent implements OnInit {
+ @Output() serverCreated = new EventEmitter<{serverName:string,serverContent:string}>();
+ @Output('bpCreated') bluePrintCreated = new EventEmitter<{serverName:string,serverContent:string}>();
   newServerName = '';
   newServerContent = '';
-  
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
    
   }
     onAddServer() {
-      // this.serverElements.push({
-      //   type: 'server',
-      //   name: this.newServerName,
-      //   content: this.newServerContent
-      // });
+      this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
     }
   
     onAddBlueprint() {
-      // this.serverElements.push({
-      //   type: 'blueprint',
-      //   name: this.newServerName,
-      //   content: this.newServerContent
-      // });
+      this.bluePrintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent});
     }
   } 
 
