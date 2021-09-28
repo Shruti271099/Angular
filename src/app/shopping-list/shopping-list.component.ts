a3111660f7b2cfb496291b1c5054a2ddb0e0c256
Shruti Lalotra <shrutilalotra453@gmail.com>
2021-09-28 09:19:59 +0530

    057-061-Project Complete

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,3 +1,4 @@
+import { ingredients } from './../shared/ingredients.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients:ingredients[] = [
+    new ingredients('Apples', 5),
+    new ingredients('Apples', 5)
+  ];
 
   constructor() { }
 
